977f1ae290f4ec683183056f6c4946a2ba5b633e
Luan Vitor Casali Dallabrida <Luanvitorcd@gmail.com>
2025-03-31 22:12:53 -0300

    ADD: FootBar animations

--- PI3_COVA/frontend/components/FooterBar/FooterBar.jsx
+++ PI3_COVA/frontend/components/FooterBar/FooterBar.jsx
@@ -1,24 +1,35 @@
+import { useState } from "react";
+import { IoHome, IoPerson, IoReceiptSharp, IoSettingsSharp, IoChatbubbles } from "react-icons/io5";
+import styles from "./styles.module.scss";
 
-    import { IoHome } from "react-icons/io5";
-    import { IoPerson } from "react-icons/io5";
-    import { IoReceiptSharp } from "react-icons/io5";
-    import { IoSettingsSharp } from "react-icons/io5";
-    import { IoChatboxEllipses } from "react-icons/io5";
-    import { IoChatbubbles } from "react-icons/io5";
-    import styles from "./styles.module.scss";
+const FooterBar = () => {
+  const [activeIndex, setActiveIndex] = useState(null);
 
-    const FooterBar = () => {
-    return (
-        <footer className={styles.FooterBar}>
-        <ul className={styles.bar}>
-            <li> <IoReceiptSharp/> </li>
-            <li> <IoChatbubbles/> </li>
-            <li> <IoHome/> </li>
-            <li> <IoPerson/> </li>
-            <li> <IoSettingsSharp/> </li>
-            </ul>
-        </footer>
-    );
-    };
+  const handleSelect = (index) => {
+    setActiveIndex(index);
+  };
 
-    export default FooterBar;
+  return (
+    <footer className={styles.FooterBar}>
+      <ul className={styles.bar}>
+        {[
+          <IoReceiptSharp />,
+          <IoChatbubbles />,
+          <IoHome />,
+          <IoPerson />,
+          <IoSettingsSharp />
+        ].map((icon, index) => (
+          <li
+            key={index}
+            className={activeIndex === index ? styles.selected : ""}
+            onClick={() => handleSelect(index)}
+          >
+            {icon}
+          </li>
+        ))}
+      </ul>
+    </footer>
+  );
+};
+
+export default FooterBar;
