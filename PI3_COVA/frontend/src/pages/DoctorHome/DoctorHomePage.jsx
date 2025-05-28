9f0496d1e917759d3ffeda3d60cb7c51c65e21fa
LuanVitorCD <luanvitorcd@gmail.com>
2025-05-28 05:27:45 -0300

    FIX: FilterBtn mobile aligned with Diagnosis text and DoctorHome now uses the doctor's name

--- PI3_COVA/frontend/src/pages/DoctorHome/DoctorHomePage.jsx
+++ PI3_COVA/frontend/src/pages/DoctorHome/DoctorHomePage.jsx
@@ -1,16 +1,20 @@
-import { useState } from 'react';
 import { useScreenResize } from '../../contexts/ScreenResizeProvider/ScreenResizeProvider';
 import styles from './styles.module.scss';
+import { useAccount } from '../../contexts/Account/AccountProvider';
+import { useTranslation } from 'react-i18next';
 
 const DoctorHomePage = () => {
   // eslint-disable-next-line no-unused-vars
   const { isMobile } = useScreenResize();
-  const [nomeMedico] = useState('Dr. Silva');
+  const { userData, loading } = useAccount();
+  const { t } = useTranslation();
+
+  if (loading) return <div>{t("common.loading")}</div>;
 
   return (
     <div className={styles.container}>
       <header className={styles.header}>
-        <h1>Bem-vindo de volta, {nomeMedico}</h1>
+        <h1>Bem-vindo de volta, {userData?.name?.split(" ").slice(0, 4).join(" ") ?? t("home.user")}</h1>
       </header>
     </div>
   );
